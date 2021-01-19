f471eac6341ed93075ff1e8673ec462ea02aeb12
Alvaro <astaboada7@gmail.com>
2021-01-19 12:30:18 +0100

    navbar with dropdown done, not the style tough

--- app/Http/Livewire/Header.php
+++ app/Http/Livewire/Header.php
@@ -6,6 +6,9 @@ use Livewire\Component;
 
 class Header extends Component
 {
+    public $partyStyles = ['Rock', 'Techno', 'Reggae', 'Boring'];
+    public $showPartyStyles = false;
+     
     public function render()
     {
         return view('livewire.header');
--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -1,3 +1,30 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <img>
+    <h1>Fiestas a la Carta</h1>
+    <h3>La Calaka</h3>
+    <navbar>
+        @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+                <div x-data="{ showPartyStyles: false }">
+                    <button @click="showPartyStyles = true"> Filter By Styles</button>
+                    <ol id="dropdownPartyStyles" x-show="showPartyStyles" @click.away="showPartyStyles = false">
+                        @foreach($partyStyles as $partyStyle)
+                        <li>{{$partyStyle}}</li>
+                        @endforeach
+                    </ol>
+                </div>
+        @endif
+
+    </navbar>
+
 </div>
