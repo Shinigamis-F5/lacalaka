7fb9d1859681cd6eb1e23db73c614c4132bf5998
vanessa <vanessapcbarros@gmail.com>
2021-01-26 16:43:13 +0100

    style: change the ui to be in accordance to homepage

--- resources/views/livewire/visitor-party-details.blade.php
+++ resources/views/livewire/visitor-party-details.blade.php
@@ -1,49 +1,63 @@
 <div>
-    <div class="xl:w-6/12">
-        <div class="w-full h-48">
-            <img src="{{$party->cover}}" class="w-full h-full" />
-        </div>
-        <div class="flex items-center pt-5">
-            <p class="text-indigo-700 text-sm font-normal pr-3">{{$party->date}}</p>
-            <p class="text-indigo-700 text-sm font-normal border-l border-gray-300 pl-3">{{$party->time}}</p>
-        </div>
+    <div>
         <div class="xl:flex items-start py-5">
-            <div>
-                <p class="font-bold xl:text-3xl text-xl text-gray-900">{{$party->title}}</p>
+            <div class="">
+                <h2 class="font-bold text-center uppercase text-l xl:text-3xl text-xl text-gray-500">{{$party->title}}</h2>
             </div>
-
         </div>
+
+        <div class="w-full h-48  overflow-hidden">
+            <img src="{{$party->cover}}" class="w-full h-full object-cover" />
+        </div>
+        <section>
+            <p class="p-2 text-sm leading-7 mt-5 text-gray-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et quidem faciunt, ut de voluptate ponit, quod summum bonum sit aut rerum et quidem, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et quidem faciunt, ut de voluptate ponit, quod summum bonum sit aut.</p>
+        </section>
+        <section class="p-2 flex justify-between pt-5">
+            <div class="flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="24" viewBox="0 0 24 24" stroke="white">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                </svg>
+                <p class="text-white text-sm font-normal pr-3">{{$party->date}}</p>
+            </div>
+            <div class="flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="24" viewBox="0 0 24 24" stroke="white">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <p class="text-white text-sm font-normal pl-3">{{$party->time}}</p>
+            </div>
+        </section>
+
         <div class="md:flex items-center justify-between">
             <div>
                 <div class="flex items-center flex-no-wrap">
                     <div class="w-8 h-8 bg-cover bg-center rounded-full">
-                        <div class="w-full h-full rounded-full bg-gray-200"></div>
+                        <img src="https://i.pravatar.cc/200" class="w-full h-full rounded-full object-cover" />
                     </div>
-                    <div class="w-8 h-8 bg-cover rounded-full border-2 border-white -ml-2">
-                        <div class="w-full h-full rounded-full bg-gray-200"></div>
+                    <div class="w-8 h-8 bg-cover rounded-full border-1 border-white -ml-2">
+                        <img src="https://i.pravatar.cc/200" class="w-full h-full rounded-full object-cover" />
                     </div>
-                    <div class="w-8 h-8 bg-cover rounded-full border-2 border-white bg-center -ml-2">
-                        <div class="w-full h-full rounded-full bg-gray-200"></div>
+                    <div class="w-8 h-8 bg-cover rounded-full border-1 border-white bg-center -ml-2">
+                        <img src="https://i.pravatar.cc/200" class="w-full h-full rounded-full object-cover" />
                     </div>
-                    <div class="w-8 h-8 bg-cover rounded-full border-2 border-white -ml-2">
-                        <div class="w-full h-full rounded-full bg-gray-200"></div>
+                    <div class="w-8 h-8 bg-cover rounded-full border-1 border-white -ml-2">
+                        <img src="https://i.pravatar.cc/200" class="w-full h-full rounded-full object-cover" />
                     </div>
-                    <div class="w-8 h-8 bg-cover rounded-full border-2 border-white -ml-2">
-                        <div class="w-full h-full rounded-full bg-gray-200"></div>
+                    <div class="w-8 h-8 bg-cover rounded-full border-1 border-white -ml-2">
+                        <img src="https://i.pravatar.cc/200" class="w-full h-full rounded-full object-cover" />
                     </div>
-                    <div class="w-8 h-8 bg-cover rounded-full border-2 border-white -ml-2">
-                        <div class="w-full h-full rounded-full bg-gray-200"></div>
+                    <div class="w-8 h-8 bg-cover rounded-full border-1 border-white -ml-2">
+                        <img src="https://i.pravatar.cc/200" class="w-full h-full rounded-full object-cover" />
                     </div>
-                    <div class="w-8 h-8 bg-cover rounded-full border-2 border-white -ml-2">
-                        <div class="w-full h-full rounded-full bg-gray-200"></div>
+                    <div class="w-8 h-8 bg-cover rounded-full border-1 border-white -ml-2">
+                        <img src="https://i.pravatar.cc/200" class="w-full h-full rounded-full object-cover" />
                     </div>
-                    <div class="w-8 h-8 bg-cover rounded-full border-2 border-white bg-center -ml-2">
-                        <div class="w-full h-full rounded-full bg-gray-200"></div>
+                    <div class="w-8 h-8 bg-cover rounded-full border-1 border-white bg-center -ml-2">
+                        <img src="https://i.pravatar.cc/200" class="w-full h-full rounded-full object-cover" />
                     </div>
-                    <div class="w-8 h-8 bg-cover rounded-full border-2 border-white -ml-2">
-                        <div class="w-full h-full rounded-full bg-gray-200"></div>
+                    <div class="w-8 h-8 bg-cover rounded-full border-1 border-white -ml-2">
+                        <img src="https://i.pravatar.cc/200" class="w-full h-full rounded-full object-cover" />
                     </div>
-                    <div class="w-8 h-8 bg-cover rounded-full border-2 border-white -ml-2">
+                    <div class="w-8 h-8 bg-cover rounded-full border-1 border-white -ml-2">
                         <div class="w-full h-full rounded-full bg-gray-700 flex items-center justify-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
                                 <path d="M9.99967 4.16666V15.8333" stroke="white" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
@@ -53,29 +67,11 @@
                     </div>
                 </div>
             </div>
-            <div class="flex items-center xl:mt-0 mt-4">
-                <button class="bg-indigo-700 text-sm text-white px-4 py-2 flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
-                        <circle cx="3.5" cy="7" r="1.75" stroke="white" stroke-width="0.75" stroke-linecap="round" stroke-linejoin="round" />
-                        <circle cx="10.5" cy="3.5" r="1.75" stroke="white" stroke-width="0.75" stroke-linecap="round" stroke-linejoin="round" />
-                        <circle cx="10.5" cy="10.5" r="1.75" stroke="white" stroke-width="0.75" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M5.0752 6.24166L8.9252 4.25833" stroke="white" stroke-width="0.75" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M5.0752 7.75833L8.9252 9.74166" stroke="white" stroke-width="0.75" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    <span class="ml-2">Share Event</span>
-                </button>
-                <button class="bg-gray-700 text-sm text-white ml-3 px-4 py-2 flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
-                        <circle cx="5.25033" cy="4.08333" r="2.33333" stroke="white" stroke-width="0.75" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M1.75 12.25V11.0833C1.75 9.79467 2.79467 8.75 4.08333 8.75H6.41667C7.70533 8.75 8.75 9.79467 8.75 11.0833V12.25" stroke="white" stroke-width="0.75" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M9.33301 6.04166C9.1259 6.04166 8.95801 6.20956 8.95801 6.41666C8.95801 6.62377 9.1259 6.79166 9.33301 6.79166V6.04166ZM12.833 6.79166C13.0401 6.79166 13.208 6.62377 13.208 6.41666C13.208 6.20956 13.0401 6.04166 12.833 6.04166V6.79166ZM11.458 4.66666C11.458 4.45956 11.2901 4.29166 11.083 4.29166C10.8759 4.29166 10.708 4.45956 10.708 4.66666H11.458ZM10.708 8.16666C10.708 8.37377 10.8759 8.54166 11.083 8.54166C11.2901 8.54166 11.458 8.37377 11.458 8.16666H10.708ZM9.33301 6.79166H12.833V6.04166H9.33301V6.79166ZM10.708 4.66666V8.16666H11.458V4.66666H10.708Z" fill="white" />
-                    </svg>
-                    <span class="ml-2">Book a Seat</span>
+            <div class="flex items-center xl:mt-0 mt-4 mb-5">
+                <button class="bg-pink-500 text-sm text-white ml-3 px-4 py-2 flex items-center">
+                    Follow
                 </button>
             </div>
-        </div>
-        <div>
-            <p class="text-sm leading-7 mt-5 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et quidem faciunt, ut de voluptate ponit, quod summum bonum sit aut rerum et quidem, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et quidem faciunt, ut de voluptate ponit, quod summum bonum sit aut.</p>
         </div>
     </div>
 </div>
--- resources/views/party-details.blade.php
+++ resources/views/party-details.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.layout')
 @section('content')
-<div class="mx-auto container xl:px-0 px-4 py-16">
+<div class="bg-gray-800 pb-5">
     @livewire('visitor-party-details', ['party' => $party])
 </div>
 @endsection
