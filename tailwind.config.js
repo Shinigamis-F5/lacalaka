427c2266a1687170ab30edfad6355492f87f16bf
vanessa <vanessapcbarros@gmail.com>
2021-01-29 08:13:24 +0100

    style:improve style on partydetails page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,29 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    purge: ['./storage/framework/views/*.php', './resources/views/**/*.blade.php'],
+    purge: [
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+    ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-                cursive: ['Salsa', 'cursive'],
-                
+                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+                cursive: ["Salsa", "cursive"],
+            },
+            boxShadow: {
+                xl:
+                    "0 1px 3px 0 rgba(180, 180, 180, 0.1), 0 1px 2px 0 rgba(180, 180, 180, 0.06)",
             },
         },
     },
 
     variants: {
         extend: {
-            opacity: ['disabled'],
+            opacity: ["disabled"],
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [require("@tailwindcss/forms")],
 };
