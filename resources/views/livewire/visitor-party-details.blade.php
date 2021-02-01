28c85507286eab9abd0771bf89f839265a9206dd
vanessa <vanessapcbarros@gmail.com>
2021-02-01 09:49:30 +0100

    change venue details route on party details page

--- resources/views/livewire/visitor-party-details.blade.php
+++ resources/views/livewire/visitor-party-details.blade.php
@@ -24,7 +24,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="34" viewBox="0 0 24 24" stroke="#f41f7b">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                         </svg>
-                        <p class="text-white text-lg md:text-xl pl-3"><a href="{{route('venue.show', $venue)}}">{{$venue->name}}</a></p>
+                        <p class="text-white text-lg md:text-xl pl-3"><a href="{{route('user.profile', $venue)}}">{{$venue->name}}</a></p>
 
                     </div>
                 </div>
