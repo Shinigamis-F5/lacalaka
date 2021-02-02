031c27bb00e957fbc612e5ebea3dbf01a14daba4
G4BR1TRZ <67324204+G4BR1TRZ@users.noreply.github.com>
2021-02-02 09:36:03 +0100

    Style Of Venue Detail

--- resources/views/venue-detail-page.blade.php
+++ resources/views/venue-detail-page.blade.php
@@ -1,34 +1,37 @@
 @extends('layouts.layout')
      @section('content')   
         <main>
-            <div class="venue_name_section">
+            <div class="venue_name_section bg-gray-800">
                 <h1>{{ $user->organization }}</h1>
             </div>
             <div class="venue_img_section">
                 <img id="venue_img" src="{{ $user->img }}" alt="Image">
             </div>
             
-            <div class="container">
-                <div class="description">
-                    <p>{{ $user->description }}</p>
-                </div>
-
-                <div class="data">
-                    <div class="address">
-                        <i class="fas fa-map-marked-alt fa-2x"></i>
-                        <span>{{ $user->address }}</span>
+            <div class="container bg-gray-800 text-white">
+                <div class="bg-black pb-1 rounded-lg">
+                    <div class="description text-gray-400 font-bold pt-5 px-4">
+                        <p>{{ $user->description }}</p>
                     </div>
-                    <div class="opening_time">
-                        <i class="fas fa-clock fa-2x"></i>
-                        <span>{{ $user->openingTimes }}</span>
+
+                    <div class="data p-5">
+                        <div class="address flex flex-col justify-content-center ">
+                            <div><i class="fas fa-map-marked-alt fa-2x w-full "></i></div>
+                            <span>{{ $user->address }}</span>
+                        </div>
+                        <div class="opening_time">
+                            <i class="fas fa-clock fa-2x w-full"></i>
+                            <span>{{ $user->openingTimes }}</span>
+                        </div>
+                    </div>
+                    <div class="bg-gray-500">
+                            <div class="hype my-2">
+                                <h1>Hype me!</h1>
+                            </div>
+                        
+                        @livewire('rate.rating', ['user' => $user])
                     </div>
                 </div>
-
-                <div class="hype">
-                    <h1>Hype me!</h1>
-                </div>
-
-                @livewire('rate.rating', ['user' => $user])
             </div>
 
         </main>
