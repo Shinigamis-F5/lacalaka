a6a30cad9b0e4a5a6b7f1d6e3ce2c68d9c00a7e3
Alvaro <astaboada7@gmail.com>
2021-02-04 18:29:16 +0100

    minor cosmetics

--- resources/views/livewire/parties-list-venue/add-party-form.blade.php
+++ resources/views/livewire/parties-list-venue/add-party-form.blade.php
@@ -12,7 +12,7 @@
 
               <div class="col-span-6 sm:col-span-3">
                 <label for="title" class="block text-sm font-medium text-gray-700">Title</label>
-                <input type="text" wire:model="title"  name="title" id="title" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required>
+                <input type="text" wire:model="title"  name="title" id="title" class="mt-1 focus:ring-rojo-light focus:border-rojo-light block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required>
               </div>
 
               <div class="col-span-6 sm:col-span-3">
@@ -38,12 +38,12 @@
 
               <div class="col-span-6 sm:col-span-3 lg:col-span-2">
                 <label for="time" class="block text-sm font-medium text-gray-700">Time</label>
-                <input type="time" wire:model="time" name="time" id="time" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required>
+                <input type="time" wire:model="time" name="time" id="time" class="mt-1 focus:ring-indigo-500 focus:border-rojo block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required>
               </div>
 
               <div class="col-span-6 sm:col-span-3">
                 <label for="style" class="block text-sm font-medium text-gray-700">Style</label>
-                <select id="style" wire:model="style" name="style"class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required>
+                <select id="style" wire:model="style" name="style"class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-rojo focus:border-rojo sm:text-sm" required>
                   <option selected>Select an option</option>
 
                   @foreach($partyStyles as $partyStyle)
@@ -56,7 +56,7 @@
           </div>
 
           <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-            <button wire:click="store" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <button wire:click="store" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-rojo hover:bg-rojo-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rojo">
               Add Party
             </button>
           </div>
--- resources/views/livewire/venue-profile.blade.php
+++ resources/views/livewire/venue-profile.blade.php
@@ -3,36 +3,36 @@
 
         <div class="flex justify-between mb-3">
             <h1 class="mb-3"><strong>Profile</strong></h1>
-            <button @click="open=true" wire:click="edit({{ $venue }})" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Edit Profile</button>
+            <button @click="open=true" wire:click="edit({{ $venue }})" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-rojo hover:bg-rojo-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rojo">Edit Profile</button>
         </div>
 
         <div x-show="open" @click.away="open = false" class="grid grid-cols-6 gap-6">
             <div class="col-span-6 sm:col-span-3">
                 <label for="organization" class="block text-sm font-medium text-gray-700">Organization</label>
-                <input type="text" wire:model="organization" name="organization" id="organization" value="{{ $venue->organization }}" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                <input type="text" wire:model="organization" name="organization" id="organization" value="{{ $venue->organization }}" class="mt-1 focus:ring-rojo focus:border-rojo block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
             </div>
             <div class="col-span-6 sm:col-span-3">
                 <label for="address" class="block text-sm font-medium text-gray-700">Address</label>
-                <input type="text" wire:model="address" name="address" id="address" value="{{ $venue->address }}" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                <input type="text" wire:model="address" name="address" id="address" value="{{ $venue->address }}" class="mt-1 focus:ring-rojo focus:rojo block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
             </div>
             <div class="col-span-6 sm:col-span-3">
                 <label for="city" class="block text-sm font-medium text-gray-700">City</label>
-                <input type="text" wire:model="city" name="city" id="city" value="{{ $venue->city }}" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                <input type="text" wire:model="city" name="city" id="city" value="{{ $venue->city }}" class="mt-1 focus:ring-rojo focus:rojo block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
             </div>
             <div class="col-span-6 sm:col-span-3">
                 <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
-                <textarea type="text" wire:model="description" name="description" id="description" value="{{ $venue->description }}" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></textarea>
+                <textarea type="text" wire:model="description" name="description" id="description" value="{{ $venue->description }}" class="mt-1 focus:ring-rojo focus:border-rojo block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></textarea>
             </div>
             <div class="col-span-6 sm:col-span-3">
                 <label for="phone" class="block text-sm font-medium text-gray-700">Phone</label>
-                <input type="text" wire:model="phone" name="phone" id="phone" value="{{ $venue->phone }}" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                <input type="text" wire:model="phone" name="phone" id="phone" value="{{ $venue->phone }}" class="mt-1 focus:ring-rojo focus:border-rojo block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
             </div>
             <div class="col-span-6 sm:col-span-3">
                 <label for="Opening Time" class="block text-sm font-medium text-gray-700">Opening Time</label>
-                <input type="time" wire:model="openingTimes" name="openingTimes" id="openingTimes" value="{{ $venue->openingTimes }}" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                <input type="time" wire:model="openingTimes" name="openingTimes" id="openingTimes" value="{{ $venue->openingTimes }}" class="mt-1 focus:ring-rojo focus:border-rojo block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
             </div>
 
-            <button @click="open=false" wire:click="upload({{ $venue }})" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Upload Profile</button>
+            <button @click="open=false" wire:click="upload({{ $venue }})" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-rojo-dark hover:bg-rojo-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rojo">Upload Profile</button>
             
         </div>
     </div>
@@ -47,14 +47,14 @@
             
 
             @if ($venue->img != null)
-                <button wire:click="deleteImg({{ $venue }})" class="mt-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Delete Photo</button>
+                <button wire:click="deleteImg({{ $venue }})" class="mt-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-rojo hover:bg-rojo-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rojo-light">Delete Photo</button>
             @else
 
                 <label for="img" class="text-sm font-medium text-gray-700">Upload Image</label>
-                <input type="file" wire:model="img" name="img" id="img" class="mt-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 accept="image/png, image/jpeg">
+                <input type="file" wire:model="img" name="img" id="img" class="mt-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-rojo hover:bg-rojo-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rojo-light accept="image/png, image/jpeg">
                 @error('img') <span class="error">{{ $message }}</span> @enderror
 
-                <button wire:click="storeImg({{ $venue }})" class="mt-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Edit Photo</button>
+                <button wire:click="storeImg({{ $venue }})" class="mt-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-rojo hover:bg-rojo-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rojo-light">Edit Photo</button>
             @endif
               
         </div>
@@ -81,7 +81,7 @@
 
             @if ($yellowCups > 0)
                 @for ($i = 0; $i < $yellowCups; $i++)
-                    <i class="fas fa-glass-martini fa-3x text-yellow-200"></i>
+                    <i class="fas fa-glass-martini fa-3x text-rojo"></i>
                     @php
                     $count-- 
                     @endphp
