acfc1a525a6777974cc647352f44089e2bb9d87c
vanessa <vanessapcbarros@gmail.com>
2021-02-04 13:52:35 +0100

    feat: add photo to profile page

--- app/Http/Livewire/UserProfile.php
+++ app/Http/Livewire/UserProfile.php
@@ -5,10 +5,15 @@ namespace App\Http\Livewire;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Livewire\WithFileUploads;
 
 class UserProfile extends Component
 {
+    use WithFileUploads;
     public $user;
+    public $img;
     
     public function mount(User $user) {
         $this->user = $user;
@@ -18,5 +23,35 @@ class UserProfile extends Component
     {
         $parties = $this->user->partiesUser()->get();
         return view('livewire.user-profile', ['parties' => $parties]);
+    }
+
+    public function saveImage() 
+    {
+        $this->validate([
+            'img' => ['required', 'image', 'max:1024'],
+        ]);
+        
+        $this->user->update([
+            'img' => $this->img->getClientOriginalName(),
+        ]);
+
+        DB::transaction( function () {
+            $this->img->storeAs('public/user-image', $this->img->getClientOriginalName());
+        });
+    
+    }
+
+     public function deleteImg(User $user)
+    {
+        $this->img = $user->img;
+
+        $user->update([
+            $user->img => null,
+        ]);
+        
+        DB::transaction( function () {
+            DB::table('users')->update(['img' => null]);
+            Storage::delete([$this->img]);
+        });
     }
 }
--- resources/views/livewire/user-profile.blade.php
+++ resources/views/livewire/user-profile.blade.php
@@ -1,15 +1,35 @@
-<div >
-    <div class="flex rounded-lg justify-between mb-3">
-        @if ($user->img == null)
-        <img class="w-1/2" src="/images/default-image.png" alt="">
-        @else
-        <img src="/user-image/{{ $user->img }}" alt="photo of {{$user->name}}">
-        @endif
-        <h2>{{$user->name}}</h2>
+<div>
+    <div class="rounded-lg mb-3">
+
+        <div class="flex">
+            @if ($user->img == null)
+            <img class="w-1/2" src="/images/default-image.png" alt="">
+            @else
+            <img class="w-1/2" src="/user-images/{{ $user->img }}" alt="">
+            @endif
+
+            <div class=" ml-4 mt-3">
+                <h2 class="font-bold text-center uppercase text-l xl:text-3xl text-xl mt-3 ">{{$user->name}}</h2>
+                @if ($user->img != null)
+                <button wire:click="deleteImg({{ $user }})" class="mt-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Delete Photo</button>
+                @else
+
+                <label for="img" class="text-sm block font-medium text-gray-700">Upload Image</label>
+                <input type="file" wire:model="img" name="img" id="img" class="mt-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 accept=" image/png, image/jpeg">
+                @error('img') <span class="error">{{ $message }}</span> @enderror
+
+                <button wire:click="storeImg({{ $user }})" class="mt-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Edit Photo
+                </button>
+                @endif
+            </div>
+        </div>
+
+
     </div>
 
     <h3>Parties that you are following</h3>
     @foreach($parties as $party)
     @livewire('party-card-homepage', ['party' => $party], key($party->id))
-    @endforeach
+    @endforeachº
 </div>
